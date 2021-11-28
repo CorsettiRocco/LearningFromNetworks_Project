92dd20597b91ccc7715ad8e0507c51b34412acc2
Rocco <corsettirocco@gmail.com>
2021-11-28 19:20:10 +0100

    Piccola correzione del file di prova dell'algoritmo approssimato

--- prova_appro.py
+++ prova_appro.py
@@ -2,12 +2,14 @@ from types import CellType
 import networkit as nk
 import time
 
+#ALGORITMO E' QUADRATICO RISPETTO AD EPSILON (inversamente, epsilon /= 10 ==> tempo * 100)
+
 G_p = nk.readGraph("prova_grafo_piccolo.edges", nk.Format.EdgeListSpaceZero)
 G_m = nk.readGraph("prova_grafo_medio.txt", nk.Format.EdgeListSpaceZero)
 
 
-btw_p = nk.centrality.ApproxBetweenness(G_p, epsilon = 0.1)
-btw_m = nk.centrality.ApproxBetweenness(G_m, epsilon = 0.1)
+btw_p = nk.centrality.ApproxBetweenness(G_p, epsilon = 0.01)
+btw_m = nk.centrality.ApproxBetweenness(G_m, epsilon = 0.01)
 
 start = time.time()
 btw_p.run()
@@ -25,11 +27,9 @@ print("Tempo per grafo piccolo = ", time_1)
 print("Tempo per grafo medio = ", time_2)
 print("Il tempo è incrementato di ", time_2 / time_1 , " volte con il grafo 10 volte più grande")
 
-print("Il grafo da 10^6 ci metterebbe circa: ",(time_2/time_1) * 100 * time_2 / 60, " minuti")
-
 G_g = nk.readGraph("prova_grafo_grande.txt", nk.Format.EdgeListSpaceZero)
 
-btw_g = nk.centrality.ApproxBetweenness(G_g, epsilon = 0.1)
+btw_g = nk.centrality.ApproxBetweenness(G_g, epsilon = 0.01)
 
 start = time.time()
 btw_g.run()
@@ -37,4 +37,4 @@ end = time.time()
 
 time_3 = end-start
 
-print("Ci ha messo = ", time_3/60, " minuti")
+print("Ci ha messo = ", time_3, " secondi")
