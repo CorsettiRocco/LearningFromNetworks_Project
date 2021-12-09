1b7d70d251ece89c1d96687e00836524f557ee77
Rocco <corsettirocco@gmail.com>
2021-12-09 11:50:32 +0100

    Solved the problem of the copy of the two lists of results. Updated the validating_rule method in order to avoid any type of output if not required. Updated Data Farming method in order to have a better view of the nodes that acquire the most influence after the elimiation.

--- Compute_Scores.py
+++ Compute_Scores.py
@@ -386,10 +386,11 @@ class Scores_Calculator:
 
         #evaluate if a majority winner exists
         if (list(self.results_voting_rule['majority_count'].values())[0] >= len(self.ranking)) and (type == 'majority_count' or 'all'):
-            print('Node: ',list(self.results_voting_rule['majority_count'].keys())[0],' wins the majority with ',\
+            if print_res:
+                print('Node: ',list(self.results_voting_rule['majority_count'].keys())[0],' wins the majority with ',\
             list(self.results_voting_rule['majority_count'].values())[0],' votes')
             self.majority_winner = int(list(self.results_voting_rule['majority_count'].keys())[0])
-        else:
+        elif print_res:
             print('No majority voting rule winner')
 
         df_voting = pd.DataFrame(self.results_voting_rule)
--- Data_Farming.py
+++ Data_Farming.py
@@ -23,19 +23,34 @@ graph_list = ["Test/Graphs/prova_grafo_piccolo.edges"]  #Add more here
 #Computation
 #I dind't know how to automatize the name choice =)
 for g in graph_list:
+    #Create a new object each time to avoid problems
     CS = Scores_Calculator()
+    #Read the graph and compute the scores and the rank
     CS.read_text_graph(g) 
     CS.set_approx(False)    #To remove in the future, after the correction of voting_rule
     CS.compute_scores()
-    results = CS.voting_rule(print_res = False)
-    print(list(results['borda_count'].keys()))
+    res = CS.voting_rule(print_res = False)
+    results = res.copy()    #To solve the copy problem
     
+    #Delete the node
     subgraph = CS.delete_nodes()
-    
+   
+    #Create a new class that takes the subgraph as input and redo calculations
     CS_sub = Scores_Calculator(graph = subgraph)
     CS_sub.set_approx(False)
     CS_sub.compute_scores()
-    results_sub = CS_sub.voting_rule(print_res = False)
+    res = CS_sub.voting_rule(print_res = False)
+    results_sub = res.copy()
+
+    #Example of results printing
+    print(list(results['borda_count'].keys()))
     print("\n",list(results_sub['borda_count'].keys()))
 
-    print("Improved:\n", improved_nodes( list(results['borda_count'].keys()), list(results_sub['borda_count'].keys()) ) )
+    #Check which nodes are the most influent after the elimination
+    improved = improved_nodes( list(results['borda_count'].keys()), list(results_sub['borda_count'].keys()) )
+    improved.sort(key = lambda imp_tuple: imp_tuple[1], reverse = True)
+    #Print the improved nodes
+    #print("Improved:\n", improved )
+    for i in improved:
+        print("Node ", i[0], " improved by ", i[1], " positions in the ranking")
+    
